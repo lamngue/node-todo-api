08b0b56ff46666ca3c51b32b298ea0386e4f4d8a
Lam Nguyen <lnguye16@gustavus.edu>
2018-10-17 08:43:47 -0500

    update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,10 @@
+const {MongoClient,ObjectID} = require('mongodb');
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db) => {
+	if(err){
+		return console.log('Unable to connect to MongoDB server');
+	}
+	console.log('Connected to MongoDB server');
+	db.collection('Users').findOneAndUpdate({_id: new ObjectID('5bc739ca0516e75b066b5e5d')},{$inc:{age: 1},$set:{name: 'Andrew'}},{returnOriginal:false}).then((res) => {
+		console.log(res);
+	});
+});
