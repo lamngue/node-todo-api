48bf6cff9fd206642bfa52b00cc4e225f690f8bb
Lam Nguyen <lnguye16@gustavus.edu>
2018-10-25 23:53:34 -0500

    test POST /todos route

--- server/server.js
+++ server/server.js
@@ -19,6 +19,7 @@ app.post('/todos',(req,res)=>{
 app.listen(3000,() => {
 	console.log('Started on port 3000');
 });
+module.exports = {app};
 //create a model
 // let newTodo = new Todo({
 // 	text: ' Edit this video  '
