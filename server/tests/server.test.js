dd4a5953a16ce99f8b177290d6ed44449f6b9ad7
Lam Nguyen <lnguye16@gustavus.edu>
2018-10-28 16:25:44 -0500

    add test for DELETE todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -85,3 +85,36 @@ describe('GET /todos/:id',() => {
 		.end(done)
 	});
 });
+describe('DELETE /todos/:id',() =>{
+	it('Should remove a todo',(done) => {
+		let hexID = todos[0]._id.toHexString();
+		request(app)
+		.delete(`/todos/${hexID}`)
+		.expect(200)
+		.expect((res) => {
+			expect(res.body.todo._id).toBe(hexID);
+		})
+		.end((err,res) => {
+			if(err){
+				return done(err);
+			}
+			Todo.findById(hexID).then((todo) => {
+				expect(todo).toNotExist();
+				done();
+			}).catch((e) => console.log(e));
+		});
+	});
+	it('Should return 404 if todo not found',(done) => {
+		let hex = new ObjectID().toHexString();
+		request(app)
+		.delete(`/todos/${hex}`)
+		.expect(200)
+		.end(done)
+	});
+	it('Should return 404 if object id is invalid',(done) => {
+		request(app)
+		.delete(`/todos/123abc`)
+		.expect(404)
+		.end(done)
+	});
+})
