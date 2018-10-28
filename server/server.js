7ad78d8ccae8b3b38931af1bac6252ac8cb6f5ed
Lam Nguyen <lnguye16@gustavus.edu>
2018-10-28 18:52:28 -0500

    add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
+const _ = require('lodash');
 var app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
@@ -51,6 +52,27 @@ app.delete('/todos/:id',(req,res) => {
 		res.send({todo});
 	}).catch((e) => {
 		return res.status(400).send();
+	});
+});
+app.patch('/todos/:id',(req,res) => {
+	const {id} = req.params;
+	const body = _.pick(req.body,['text','completed']);
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+	if(_.isBoolean(body.completed)&&body.completed){
+		body.completedAt = new Date().getTime();
+	}else{
+		body.completed = false;
+		body.completedAt = null;
+	}
+	Todo.findByIdAndUpdate(id,{$set: body},{new: true}).then((todo) => {
+		if(!todo){
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
 	});
 });
 app.listen(port,() => {
