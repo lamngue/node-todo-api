195d26653a1f31320a4b4db7c23fce40ca91c64d
Lam Nguyen <lnguye16@gustavus.edu>
2018-10-28 21:29:15 -0500

    add route POST /user

--- server/server.js
+++ server/server.js
@@ -76,6 +76,16 @@ app.patch('/todos/:id',(req,res) => {
 		res.status(400).send();
 	});
 });
+//POST /users
+app.post('/users',(req,res) => {
+	const body = _.pick(req.body,['email','password']);
+	let user = new User(body);
+	user.save().then((doc) => {
+		res.send(doc);
+	},(e) => {
+		res.status(400).send(e);
+	});
+});
 app.listen(port,() => {
 	console.log(`Started on port ${port}`);
 });
